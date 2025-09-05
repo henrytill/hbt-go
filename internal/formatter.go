1c15885771739be8c9d49566c4e728dc62877848
Henry Till <henrytill@gmail.com>
2025-09-04 22:40:39 -0700

    internal: remove vestigial {Formatter,Parser}Registry code

--- internal/formatter.go
+++ internal/formatter.go
@@ -1,7 +1,6 @@
 package internal
 
 import (
-	"fmt"
 	"io"
 
 	"github.com/henrytill/hbt-go/internal/types"
@@ -9,33 +8,4 @@ import (
 
 type Formatter interface {
 	Format(w io.Writer, collection *types.Collection) error
-}
-
-type FormatterRegistry struct {
-	formatters map[Format]Formatter
-}
-
-func NewFormatterRegistry() *FormatterRegistry {
-	return &FormatterRegistry{
-		formatters: make(map[Format]Formatter),
-	}
-}
-
-func (r *FormatterRegistry) Register(format Format, formatter Formatter) error {
-	if !format.CanOutput() {
-		return fmt.Errorf("format %s cannot be used for output", format.Name)
-	}
-	r.formatters[format] = formatter
-	return nil
-}
-
-func (r *FormatterRegistry) GetFormatter(format Format) (Formatter, error) {
-	if !format.CanOutput() {
-		return nil, fmt.Errorf("format %s cannot be used for output", format.Name)
-	}
-	formatter, exists := r.formatters[format]
-	if !exists {
-		return nil, fmt.Errorf("no formatter available for format: %s", format.Name)
-	}
-	return formatter, nil
 }
--- internal/parser.go
+++ internal/parser.go
@@ -1,7 +1,6 @@
 package internal
 
 import (
-	"fmt"
 	"io"
 
 	"github.com/henrytill/hbt-go/internal/types"
@@ -9,33 +8,4 @@ import (
 
 type Parser interface {
 	Parse(r io.Reader) (*types.Collection, error)
-}
-
-type ParserRegistry struct {
-	parsers map[Format]Parser
-}
-
-func NewParserRegistry() *ParserRegistry {
-	return &ParserRegistry{
-		parsers: make(map[Format]Parser),
-	}
-}
-
-func (r *ParserRegistry) Register(format Format, parser Parser) error {
-	if !format.CanInput() {
-		return fmt.Errorf("format %s cannot be used for input", format.Name)
-	}
-	r.parsers[format] = parser
-	return nil
-}
-
-func (r *ParserRegistry) GetParser(format Format) (Parser, error) {
-	if !format.CanInput() {
-		return nil, fmt.Errorf("format %s cannot be used for input", format.Name)
-	}
-	parser, exists := r.parsers[format]
-	if !exists {
-		return nil, fmt.Errorf("no parser available for format: %s", format.Name)
-	}
-	return parser, nil
 }
