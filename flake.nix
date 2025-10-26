19ca2db2608efd2ff47f9e99613abe4692426d2e
Henry Till <henrytill@gmail.com>
2025-10-25 20:29:23 -0700

    flake.nix: fix version

--- flake.nix
+++ flake.nix
@@ -14,35 +14,38 @@
     }:
     let
       overlay = final: prev: {
-        hbt = final.buildGoModule {
-          pname = "hbt";
-          version = "0.1.0-${self.shortRev or "dirty"}";
+        hbt =
+          let
+            version = "0.1.0-${self.shortRev or self.dirtyShortRev}";
+          in
+          final.buildGoModule {
+            pname = "hbt";
+            inherit version;
 
-          src = builtins.path {
-            path = ./.;
-            name = "hbt-src";
+            src = builtins.path {
+              path = ./.;
+              name = "hbt-src";
+            };
+
+            vendorHash = "sha256-M4SWV8N9gpc4CaQMFlI86uyPqprFI8eoe6TWrvdSfks=";
+
+            ldflags = [
+              "-X main.Version=${version}"
+            ];
+
+            preCheck = ''
+              # Set binary path for tests to find the built executable
+              export HBT_BINARY_PATH="$GOPATH/bin/hbt"
+            '';
+
+            checkFlags = [ "-v" ];
+
+            meta = with final.lib; {
+              description = "Heterogeneous Bookmark Transformation";
+              homepage = "https://github.com/henrytill/hbt-go";
+              maintainers = with maintainers; [ ];
+            };
           };
-
-          vendorHash = "sha256-M4SWV8N9gpc4CaQMFlI86uyPqprFI8eoe6TWrvdSfks=";
-
-          ldflags = [
-            "-X main.commitHash=${self.rev or self.dirtyRev}"
-            "-X main.commitShortHash=${self.shortRev or self.dirtyShortRev}"
-          ];
-
-          preCheck = ''
-            # Set binary path for tests to find the built executable
-            export HBT_BINARY_PATH="$GOPATH/bin/hbt"
-          '';
-
-          checkFlags = [ "-v" ];
-
-          meta = with final.lib; {
-            description = "Heterogeneous Bookmark Transformation";
-            homepage = "https://github.com/henrytill/hbt-go";
-            maintainers = with maintainers; [ ];
-          };
-        };
       };
     in
     flake-utils.lib.eachDefaultSystem (
